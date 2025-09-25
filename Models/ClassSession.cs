cb262b8d0f66d534a9d3c64f966749338921a10b
TranNhi67 <trannhi050180@gmail.com>
2025-09-25 20:07:20 +0700

    Modify StudentSchedule file

---
+++ Models/ClassSession.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentAttendanceMVC.Models
+{
+    public class ClassSession
+    {
+        public int Id { get; set; }
+        [Required]
+        public string CourseName { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string Time { get; set; } = string.Empty; // Thêm Time
+        public string Room { get; set; } = string.Empty; // Thêm Room
+        public List<Student> Students { get; set; } = new List<Student>();
+    }
+}
